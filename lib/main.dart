c01df860f41769adaf5247b5e3f975ffd86f0e41
PabloGO4 <pgrandeocampo13@gmail.com>
2024-10-10 23:51:51 +0200

    añadir crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:sqflite/sqflite.dart';
-import 'database/user_dao.dart';
+import 'database/user_dao.dart'; // Importar tu DAO
 import 'models/user_model.dart'; // Importar el modelo User
 
 void main() => runApp(MyApp());
@@ -11,68 +8,244 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'User Demo',
-      home: UserScreen(),
+      title: 'Gestión de Usuarios',
+      home: HomeScreen(),
     );
   }
 }
 
-class UserScreen extends StatefulWidget {
+// Pantalla de Inicio con las opciones
+class HomeScreen extends StatelessWidget {
   @override
-  _UserScreenState createState() => _UserScreenState();
-}
-
-class _UserScreenState extends State<UserScreen> {
-  List<User> usuarios = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUsers();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menú Principal'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildMenuButton(context, 'Listar Base de Datos', ListUsersScreen()),
+            SizedBox(height: 20),
+            _buildMenuButton(context, 'Crear Usuario', CreateUserScreen()),
+            SizedBox(height: 20),
+            _buildMenuButton(context, 'Eliminar Usuario', DeleteUserScreen()),
+            SizedBox(height: 20),
+            _buildMenuButton(context, 'Modificar Usuario', UpdateUserScreen()),
+            SizedBox(height: 20),
+            _buildMenuButton(context, 'Consultas', QueryScreen()),
+          ],
+        ),
+      ),
+    );
   }
 
-  Future<void> _loadUsers() async {
-    List<User> users = await UserDao().getUsers();
+  // Botones centrados y del mismo tamaño
+  Widget _buildMenuButton(BuildContext context, String label, Widget screen) {
+    return Center(
+      child: SizedBox(
+        width: double.infinity, // Hacer que los botones ocupen todo el ancho
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+          },
+          child: Text(label),
+        ),
+      ),
+    );
+  }
+}
+
+// Pantalla para Crear Usuario
+class CreateUserScreen extends StatefulWidget {
+  @override
+  _CreateUserScreenState createState() => _CreateUserScreenState();
+}
+
+class _CreateUserScreenState extends State<CreateUserScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _apellido1Controller = TextEditingController();
+  final TextEditingController _apellido2Controller = TextEditingController();
+  final TextEditingController _telefonoController = TextEditingController();
+  final TextEditingController _dniController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  String? _provinciaSeleccionada;
+  List<String> provincias = [
+    'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 
+    'Barcelona', 'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 
+    'Córdoba', 'Cuenca', 'Gerona', 'Granada', 'Guadalajara', 'Guipúzcoa', 'Huelva', 
+    'Huesca', 'Islas Baleares', 'Jaén', 'La Coruña', 'La Rioja', 'Las Palmas', 'León', 
+    'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Murcia', 'Navarra', 'Orense', 'Palencia', 
+    'Pontevedra', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 
+    'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 
+    'Zamora', 'Zaragoza'
+  ];
+
+  // Función para guardar el usuario
+  Future<void> _saveUser() async {
+    if (_formKey.currentState!.validate()) {
+      User newUser = User(
+        nombre: _nombreController.text,
+        apellido1: _apellido1Controller.text,
+        apellido2: _apellido2Controller.text,
+        telefono: _telefonoController.text,
+        dni: _dniController.text,
+        email: _emailController.text,
+        provincia: _provinciaSeleccionada!,
+      );
+
+      // Insertar el usuario en la base de datos usando UserDao
+      await UserDao().insertUser(newUser);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Usuario guardado con éxito')),
+      );
+
+      // Limpiar los campos del formulario
+      _nombreController.clear();
+      _apellido1Controller.clear();
+      _apellido2Controller.clear();
+      _telefonoController.clear();
+      _dniController.clear();
+      _emailController.clear();
+      setState(() {
+        _provinciaSeleccionada = null;
+      });
+    }
+  }
+
+  void _clearFields() {
+    _nombreController.clear();
+    _apellido1Controller.clear();
+    _apellido2Controller.clear();
+    _telefonoController.clear();
+    _dniController.clear();
+    _emailController.clear();
     setState(() {
-      usuarios = users;
+      _provinciaSeleccionada = null;
     });
-  }
-
-  Future<void> _addUser() async {
-    User newUser = User(
-      nombre: 'Carlos',
-      apellido1: 'Rodríguez',
-      apellido2: 'Gómez',
-      telefono: '654321987',
-      dni: '12345678A',
-      email: 'carlos@example.com',
-      provincia: 'Madrid',
-    );
-    await UserDao().insertUser(newUser);
-    _loadUsers(); // Cargar los usuarios actualizados
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Usuarios'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: _addUser,
+        title: Text('Crear Usuario'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: _nombreController,
+                decoration: InputDecoration(labelText: 'Nombre'),
+              ),
+              TextFormField(
+                controller: _apellido1Controller,
+                decoration: InputDecoration(labelText: 'Primer Apellido'),
+              ),
+              TextFormField(
+                controller: _apellido2Controller,
+                decoration: InputDecoration(labelText: 'Segundo Apellido'),
+              ),
+              TextFormField(
+                controller: _telefonoController,
+                decoration: InputDecoration(labelText: 'Teléfono'),
+              ),
+              TextFormField(
+                controller: _dniController,
+                decoration: InputDecoration(labelText: 'DNI'),
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Correo Electrónico'),
+              ),
+              DropdownButtonFormField<String>(
+                value: _provinciaSeleccionada,
+                items: provincias.map((String provincia) {
+                  return DropdownMenuItem<String>(
+                    value: provincia,
+                    child: Text(provincia),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _provinciaSeleccionada = newValue;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Provincia'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: _clearFields,
+                    child: Text('Cancelar'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveUser,
+                    child: Text('Guardar'),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-      body: ListView.builder(
-        itemCount: usuarios.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(usuarios[index].nombre),
-            subtitle: Text(usuarios[index].email),
-          );
-        },
-      ),
+    );
+  }
+}
+
+// Pantalla para listar usuarios
+class ListUsersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Listar Usuarios')),
+      body: Center(child: Text('Aquí se listarán los usuarios')),
+    );
+  }
+}
+
+// Pantalla para eliminar usuarios
+class DeleteUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Eliminar Usuario')),
+      body: Center(child: Text('Aquí se eliminará un usuario')),
+    );
+  }
+}
+
+// Pantalla para modificar usuarios
+class UpdateUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Modificar Usuario')),
+      body: Center(child: Text('Aquí se modificará un usuario')),
+    );
+  }
+}
+
+// Pantalla para consultas (placeholder)
+class QueryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Consultas')),
+      body: Center(child: Text('Aquí se realizarán consultas')),
     );
   }
 }
